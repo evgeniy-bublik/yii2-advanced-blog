eb7c700cac11d1584c01b2fb91810816a03eef79
Evgeniy Bublik <jeka.deadline@gmail.com>
2017-12-08 23:29:38 +0200

    новый шаблон для клиентской части

--- frontend/themes/yamen/layouts/inc/preloader.php
+++
@@ -1,7 +0,0 @@
-<!-- Start Loading -->
-<section class="loading-overlay">
-    <div class="Loading-Page">
-        <h2 class="loader">Loading...</h2>
-    </div>
-</section>
-<!-- End Loading  -->
--- frontend/themes/yamen/modules/core/views/index/catch-all.php
+++
@@ -1,5 +0,0 @@
-<section class="content wrapper">
-    <h1>Скоро в сети появится еще один блог</h1>
-    <p class="description">Совсем скоро появится еще один блог в котором будут самые интересные и познавательные статьи из it сферы</p>
-    <div class="counter clear" data-date="2018-01-01 00:00:00"></div>
-</section>
