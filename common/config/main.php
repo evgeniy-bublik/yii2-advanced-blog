99814b37608c8ac939e0f4aa0f069099feae8345
Evgeniy <jeka.deadline@gmail.com>
2017-12-11 12:39:33 +0200

    Изменения в конфигурации

--- common/config/main.php
+++ common/config/main.php
@@ -1,9 +1,13 @@
 <?php
 return [
+    'timeZone' => 'Europe/Kiev',
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
+        ],
+        'formatter' => [
+            'defaultTimeZone' => 'Europe/Kiev',
         ],
     ],
 ];
